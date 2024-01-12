ecf0ecf5ad3fae428665d9ec6da91c09df5666b3
RabinThapa1998 <imrabin1998@gmail.com>
2024-01-12 15:41:37 +0545

    update lightpaper page

--- src/app/lightpaper/page.tsx
+++ src/app/lightpaper/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+    return (
+        <div className="my-32">
+            <h1 className="text-2xl text-center font-bold mb-7">LightPaper</h1>
 
-export default page
+            <iframe
+                src="https://adsflow.s3.amazonaws.com/AdsFlow_Lightpaper_V1.pdf"
+                className="h-[80vh] max-w-screen-lg w-full mx-auto"
+                title="AdsFlow Lightpaper"
+            ></iframe>
+        </div>
+    );
+};
+
+export default page;
